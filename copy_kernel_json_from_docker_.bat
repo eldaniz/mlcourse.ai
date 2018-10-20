3566d2360ff1b25f4625c3b0c821062113284196
AEGo <eldaniz@abasov.ru>
2018-10-20 12:42:16 +0300

    cosmetics

--- copy_kernel_json_from_docker_.bat
+++ copy_kernel_json_from_docker_.bat
@@ -1,5 +1,4 @@
 set scp_cmd=C:\Proj\Utils\scp\WinSCP.exe
-rem %scp_cmd% /log=winscp.log root:12345@10.0.75.2:5022
 %scp_cmd% /log=winscp.log root:12345@10.0.75.2:5022 /command "get /root/.local/share/jupyter/runtime/*.json D:\DiskD\Eldaniz\ML\MLCourse\remote_kernels\"
 
 
--- docker_ssh_start.bat
+++ docker_ssh_start.bat
@@ -1,4 +1,2 @@
-rem docker exec -it %1 /bin/bash
-
 docker exec -it aego service ssh restart
 docker exec -it aego python3 -m spyder_kernels.console
